9c00239b27daba932f30f926c7bb2269a0725e3c
FabienB4 <fabienb4.b4soft@gmail.com>
2015-03-17 20:07:43 -0600

    Bumped version to 0.9.5 (now aligned with arunoda/meteor-up).
    Arunoda's updates:
    Install npm modules before doing binary rebuild
    Update README.md
    Improve binary module building logic
    Verify SSL config
    Add missing sudo

--- templates/linux/deploy.sh
+++ templates/linux/deploy.sh
@@ -4,17 +4,40 @@
 gyp_rebuild_inside_node_modules () {
   for npmModule in ./*; do
     cd $npmModule
-    if [ -f binding.gyp ]; then
-      echo "=> re-installing binary npm module '${npmModule:2}' of package '${package:2}'"
-      sudo node-gyp rebuild
+
+    isBinaryModule="no"
+    # recursively rebuild npm modules inside node_modules
+    check_for_binary_modules () {
+      if [ -f binding.gyp ]; then
+        isBinaryModule="yes"
+      fi
+
+      if [ $isBinaryModule != "yes" ]; then
+        if [ -d ./node_modules ]; then
+          cd ./node_modules
+          for module in ./*; do
+            cd $module
+            check_for_binary_modules
+            cd ..
+          done
+          cd ../
+        fi
+      fi
+    }
+
+    check_for_binary_modules
+
+    if [ $isBinaryModule == "yes" ]; then
+      echo " > $npmModule: npm install due to binary npm modules"
+      rm -rf node_modules
+      if [ -f binding.gyp ]; then
+        sudo npm install
+        sudo node-gyp rebuild || :
+      else
+        sudo npm install
+      fi
     fi
 
-    # recursively rebuild npm modules inside node_modules
-    if [ -d ./node_modules ]; then
-      cd ./node_modules
-        gyp_rebuild_inside_node_modules
-      cd ../
-    fi
     cd ..
   done
 }
